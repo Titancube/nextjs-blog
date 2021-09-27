66a08ec774447867fd535ef360f8909f899be589
titancube <kbin0717@gmail.com>
2021-09-27 18:03:58 +0900

    refactor: convert to typescript

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+export const purge = [
+  "./pages/**/*.js",
+  "./pages/**/*.tsx",
+  "./components/**/*.js",
+  "./components/**/*.tsx",
+];
+export const darkMode = false;
+export const theme = {
+  extend: {},
+};
+export const variants = {
+  extend: {},
+};
+export const plugins = [];
