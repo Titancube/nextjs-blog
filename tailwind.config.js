3318e116bcb23eb79402d0b1a1cfd08d2fb9635a
titancube <kbin0717@gmail.com>
2021-09-27 17:46:51 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    "./pages/**/*.js",
+    "./pages/**/*.tsx",
+    "./components/**/*.js",
+    "./components/**/*.tsx",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
